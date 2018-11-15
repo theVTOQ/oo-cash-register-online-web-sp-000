c338fa8591d529321e369fcbaf01a4ee213e8ae5
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-15 18:54:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,14 @@
 class CashRegister
-  attr_accessor :total
+  attr_accessor :total, :items
 
   def initialize(discount = 0)
     @total = 0
     @total -= discount
+    @items = []
   end
 
   def add_item(item, price, quantity = 1)
     @total += price*quantity
+    quantity.times {@items << item}
   end
 end
